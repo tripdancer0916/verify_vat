5f7834455f11ae2ff700ac7be2d8c567f4d3a3f2
aiinside-6 <tripdancer0916@gmail.com>
2018-06-15 17:27:43 +0900

    change epochs

--- conv-large_with_vat.py
+++ conv-large_with_vat.py
@@ -20,7 +20,7 @@ from functools import reduce
 
 batch_size = 32
 num_classes = 10
-epochs = 100
+epochs = 200
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
