0202850b9c1af8ac5cb82b57e16549584ece9e2a
aiinside-6 <tripdancer0916@gmail.com>
2018-06-18 09:58:34 +0900

    experiment

--- conv-large.py
+++ conv-large.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 import keras
+from keras import backend as K
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.engine.topology import Input, Container
@@ -12,9 +13,13 @@ from keras.layers import Dense, Dropout, Activation, Flatten, advanced_activatio
 from keras.layers import Conv2D, MaxPooling2D, Convolution2D, pooling
 import os
 
-batch_size = 32
+import numpy as np
+
+from functools import reduce
+
+batch_size = 100
 num_classes = 10
-epochs = 100
+epochs = 300
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
@@ -25,9 +30,41 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# Convert class vectors to binary class matrices.
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+def normalize_vector(x):
+    z = K.sum(K.batch_flatten(K.square(x)), axis=1)
+    while K.ndim(z) < K.ndim(x):
+        z = K.expand_dims(z, axis=-1)
+    return x / (K.sqrt(z))
+
+
+def kld_(p, q):
+    v = p * (K.log(p + K.constant(1e-8)) - K.log(q + K.constant(1e-8)))
+    return K.sum(K.batch_flatten(v), axis=1, keepdims=True)
+
+
+def loss_with_vat(target, output):
+    normal_outputs = [K.stop_gradient(x) for x in model.outputs]
+    d_list = [K.random_normal((32, 32, 3))] * batch_size
+    ip = 1
+    xi = 10
+    eps = 2
+    for _ in range(ip):
+        new_inputs = [x + normalize_vector(d) * xi for (x, d) in zip(model.inputs, d_list)]
+        new_outputs = [model.call(new_inputs)]
+        klds = [K.sum(kld_(normal, new)) for normal, new in zip(normal_outputs, new_outputs)]
+        kld = reduce(lambda t, x: t + x, klds, 0)
+        d_list = [K.stop_gradient(d) for d in K.gradients(kld, d_list)]
+
+    new_inputs = [x + normalize_vector(d) * eps for (x, d) in zip(model.inputs, d_list)]
+    y_perturbations = model.call(new_inputs)
+    klds = [K.mean(kld_(normal, new)) for normal, new in zip(normal_outputs, [y_perturbations])]
+    kld = reduce(lambda t, x: t + x, klds, 0)
+    return K.categorical_crossentropy(target, output) + kld / batch_size
+
 
 input_layer = Input(x_train.shape[1:])
 x = Convolution2D(128, (3, 3), padding='same')(input_layer)
@@ -36,7 +73,7 @@ x = advanced_activations.LeakyReLU(alpha=0.1)(x)
 x = Convolution2D(128, (3, 3), padding='same')(x)
 x = BatchNormalization()(x)
 x = advanced_activations.LeakyReLU(alpha=0.1)(x)
-x = Convolution2D(128, (3, 3), padding='same')(x)
+x = Convolution2D(128, (3, 3), padding='valid')(x)
 x = BatchNormalization()(x)
 x = advanced_activations.LeakyReLU(alpha=0.1)(x)
 
@@ -49,7 +86,7 @@ x = advanced_activations.LeakyReLU(alpha=0.1)(x)
 x = Convolution2D(256, (3, 3), padding='same')(x)
 x = BatchNormalization()(x)
 x = advanced_activations.LeakyReLU(alpha=0.1)(x)
-x = Convolution2D(256, (3, 3), padding='same')(x)
+x = Convolution2D(256, (3, 3), padding='valid')(x)
 x = BatchNormalization()(x)
 x = advanced_activations.LeakyReLU(alpha=0.1)(x)
 
@@ -62,7 +99,7 @@ x = advanced_activations.LeakyReLU(alpha=0.1)(x)
 x = Convolution2D(256, (1, 1), padding='same')(x)
 x = BatchNormalization()(x)
 x = advanced_activations.LeakyReLU(alpha=0.1)(x)
-x = Convolution2D(128, (1, 1), padding='same')(x)
+x = Convolution2D(128, (1, 1), padding='valid')(x)
 x = BatchNormalization()(x)
 x = advanced_activations.LeakyReLU(alpha=0.1)(x)
 
@@ -71,7 +108,15 @@ x = pooling.GlobalAveragePooling2D()(x)
 output = Dense(10, activation="softmax")(x)
 
 
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+def learning_rates(epoch):
+    if epoch < epochs / 2:
+        return 0.003
+    else:
+        return np.linspace(0.003, 0.0005, int(epochs / 2))[epoch]
+
+
+opt = keras.optimizers.Adam(lr=0.003, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+lr_cb = keras.callbacks.LearningRateScheduler(lambda epoch: float(learning_rates(epoch)))
 
 model = Model(input_layer, output)
 model.summary()
